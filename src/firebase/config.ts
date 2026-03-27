7b284dffcf3f43b8f8f126354b91ae2aa9d555c6
hamidtazi79 <hamidboukhriss02@gmail.com>
2026-03-27 19:25:52 +0000

    Update config.ts

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,9 @@
 export const firebaseConfig = {
-  projectId: "studio-7785401397-f3878",
-  appId: "1:110702874229:web:864e48524811fedd50723c",
-  storageBucket: "studio-7785401397-f3878.appspot.com",
-  apiKey: "AIzaSyBRZZ5wJ2YpMwNDAHGAlNhOFQ1T6FdNkqk",
-  authDomain: "studio-7785401397-f3878.firebaseapp.com",
-  messagingSenderId: "110702874229",
-  measurementId: "G-XXXXXXXXXX",
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID!,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET!,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID!,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID!,
 };
